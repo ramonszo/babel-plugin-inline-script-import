42341c05d7cf73d22406c7e18f207c96f6795a48
Ꝛ <me@ramon82.com>
2019-06-25 18:18:21 -0300

    Update

--- build/index.js
+++ build/index.js
@@ -1,21 +1,15 @@
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
   value: true
 });
 
-exports.default = babelPluginInlineScriptImport;
-
-const babel = require('babel-core');
 const babylon = require('babylon');
 const babelTraverse = require('babel-traverse').default;
 
-const _path = require('path');
-const _path2 = _interopRequireDefault(_path);
-const _decache = require('decache');
-const _decache2 = _interopRequireDefault(_decache);
+const _interopRequireDefault = (obj) => {
+  return obj && obj.__esModule ? obj : { default: obj };
+};
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function formatName(filename){
+const formatName = (filename) => {
   const path = require('path');
   let name = filename;
 
@@ -25,49 +19,54 @@ function formatName(filename){
     name = path.basename(path.dirname(filename));
   }
 
-  //name = normalizeName(name);  
-
   return name;
-}
+};
 
-function getFile(filepath) {
+const getFile = (filepath) => {
   (0, _decache2.default)(filepath);
 
   const fs = require('fs');
   const code = fs.readFileSync(filepath, 'utf8');
 
-  //return code;
-  
-  return babylon.parse(code, {
-    sourceType: "module"
-  });
-}
+  // if log --babylon
+  // console.log('[Babylon]', filepath);
+  // return code;
 
-function babelPluginInlineScriptImport(_ref) {
+  return babylon.parse(code, {
+    sourceType: 'module'
+  });
+};
+
+const _path = require('path');
+const _path2 = _interopRequireDefault(_path);
+const _decache = require('decache');
+const _decache2 = _interopRequireDefault(_decache);
+
+exports.default = (_ref) => {
   const t = _ref.types;
 
   return {
     visitor: {
       ExportDeclaration: {
-        exit: function exit(path, state) {
-          const name = formatName(state.file.opts.filename);          
+        exit: (path, state) => {
+          const name = formatName(state.file.opts.filename);
           let declaration = path.node.declaration;
 
-          if(!declaration){
+          if (!declaration) {
             return;
           }
 
-          if (declaration.id && declaration.id.name) {            
+          if (declaration.id && declaration.id.name) {
             return;
           }
 
           if (t.isExportDefaultDeclaration(path.node)) {
-            declaration = t.variableDeclaration("var", [
-                                  t.variableDeclarator(
-                                    t.identifier(name), 
+            declaration = t.variableDeclaration('var', [
+              t.variableDeclarator(
+                                    t.identifier(name),
                                     declaration
                                   )
-                                ]);            
+            ]);
           }
 
           path.replaceWith(declaration);
@@ -75,19 +74,19 @@ function babelPluginInlineScriptImport(_ref) {
       },
 
       ImportDeclaration: {
-        exit: function exit(path, state) {
+        exit: (path, state) => {
           const node = path.node;
           const modulePath = node.source.value;
           const specificImports = [];
 
           node.specifiers.forEach((spec) => {
-            let filepath = null;            
+            let filepath = null;
             let isWildcard = false;
             let currentExpression = t.booleanLiteral(false);
 
-            if(t.isImportNamespaceSpecifier(spec)){
+            if (t.isImportNamespaceSpecifier(spec)) {
               isWildcard = true;
-            } else if (!t.isImportSpecifier(spec)&&!t.isImportDefaultSpecifier(spec)) {
+            } else if (!t.isImportSpecifier(spec) && !t.isImportDefaultSpecifier(spec)) {
               return;
             }
 
@@ -99,23 +98,25 @@ function babelPluginInlineScriptImport(_ref) {
             }
 
             let defaultSpecifier = false;
-            if(t.isImportDefaultSpecifier(spec)){
+            if (t.isImportDefaultSpecifier(spec)) {
               defaultSpecifier = spec;
             }
 
             const ast = getFile(filepath);
             const body = ast.program.body;
             const varName = spec.local.name;
-            const moduleName = (spec.imported)?spec.imported.name:'*';
-            const defaultName = formatName(filepath);    
+            const moduleName = (spec.imported) ? spec.imported.name : '*';
+            const defaultName = formatName(filepath);
             const allExports = {};
 
             babelTraverse(ast, {
-              enter(path) {
-                //console.log('search import '+moduleName+' as '+varName+' from '+filepath);
+              enter (path) {
+                /// /console.log('search import '+moduleName+' as '+varName+' from '+filepath);
 
-                if (path.isExportDefaultDeclaration()){
-                  if(moduleName=='default') {
+                if (path.isExportDefaultDeclaration()) {
+                  // console.log('export default', path.node);
+
+                  if (moduleName == 'default' || moduleName == '*') {
                     currentExpression = path.node.declaration;
 
                     allExports['default'] = path.node.declaration;
@@ -124,34 +125,55 @@ function babelPluginInlineScriptImport(_ref) {
                   const vars = path.node.declaration;
                   let nameDeclaration = false;
 
-                  if(vars) {     
+                  // console.log('export type', vars);
+                  if (vars) {
                     nameDeclaration = vars.declarations[0].id.name;
 
-                    if(nameDeclaration==moduleName) {
+                    if (nameDeclaration == moduleName) {
                       currentExpression = vars.declarations[0].init;
-                    } else if (defaultSpecifier){
-                      if(vars.declarations[0].id.name==defaultSpecifier.local.name) {
+                    } else if (defaultSpecifier) {
+                      if (vars.declarations[0].id.name == defaultSpecifier.local.name) {
                         currentExpression = vars.declarations[0].init;
                       }
                     }
 
+                    // console.log('export', nameDeclaration);
+
                     allExports[nameDeclaration] = vars.declarations[0].init;
-
-                  } else if(path.node.specifiers.length > 0){
-                    //remove specifiers
                   }
+                } else if (path.isFunctionDeclaration()) {
+                  // const vars = path.node.declaration;
+                  let declaration = path.node;
+                  let nameDeclaration = false;
+                  let expression = currentExpression;
 
+                  // console.log('isfunction');
+                  if (declaration) {
+                    nameDeclaration = declaration.id.name;
+                    expression = t.functionExpression(path.node.id, path.node.params, path.node.body, path.node.generator, path.node.async);
+
+                    if (nameDeclaration == moduleName) {
+                      currentExpression = expression;
+                    }
+
+                    // console.log('func', nameDeclaration, Object.keys(path.node.body), path.node.params);
+
+                    allExports[nameDeclaration] = expression;
+                    // allExports[nameDeclaration] = declaration;
+                  }
+                } else {
+                  // console.log('what is ', path.node.type);
                 }
               }
             });
 
-            if(isWildcard){
-              objectMembers = [];
+            if (isWildcard) {
+              var objectMembers = [];
 
-              Object.keys(allExports).forEach( (key) => {
+              Object.keys(allExports).forEach((key) => {
                 objectMembers.push(
                   t.objectProperty(
-                    t.identifier(key), 
+                    t.identifier(key),
                     allExports[key]
                   )
                 );
@@ -160,24 +182,24 @@ function babelPluginInlineScriptImport(_ref) {
               currentExpression = t.objectExpression(objectMembers);
             }
 
-            const declaration = t.variableDeclaration("var", [
-                                  t.variableDeclarator(
-                                    t.identifier(varName), 
+            const declaration = t.variableDeclaration('var', [
+              t.variableDeclarator(
+                                    t.identifier(varName),
                                     currentExpression
                                   )
-                                ]);
+            ]);
 
             specificImports.push(declaration);
           });
 
           if (specificImports.length) {
             path.replaceWithMultiple(specificImports);
-          } else {
+          } /* else {
             console.log(node);
           }
-
+          */
         }
       }
     }
   };
-}
+};
